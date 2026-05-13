b8aa577bfd76613bf744f47ba646f27885b55f61
Jamie <75079031+sirjahames@users.noreply.github.com>
2026-05-13 01:45:12 -0500

    Remove redundant try & catch

--- backend/src/api/animals/animals.model.ts
+++ backend/src/api/animals/animals.model.ts
@@ -2,12 +2,8 @@ import { prisma } from "@/lib/prisma.js";
 import type { Animal } from "@/types/animal.js";
 
 async function getAnimals(): Promise<Animal[]> {
-    try {
-        const animals = await prisma.animal.findMany();
-        return animals;
-    } catch (error) {
-        throw error;
-    }
+    const animals = await prisma.animal.findMany();
+    return animals;
 }
 
 export {
