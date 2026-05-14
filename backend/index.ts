4494b335b8aa4766a80fdc4dfb46c888c1ad28ea
Jamie <75079031+sirjahames@users.noreply.github.com>
2026-05-13 22:09:25 -0500

    Disconnect prisma on SIGINT and SIGTERM and introduce tsc-alias for tsc build

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,20 @@
-import server from "./server.js";
+import { prisma } from "~/src/lib/prisma/prisma.js";
+import server from "~/src/server.js";
 
 process.on("SIGINT", () => {
     server.close();
+    prisma.$disconnect();
     process.exit(0);
 });
 
 process.on("SIGTERM", () => {
+    prisma.$disconnect();
     server.close((error) => {
         if (error) {
             console.error(error);
             process.exit(1);
         }
 
-        console.log(`Server closed successfully!`);
         process.exit(0);
     });
 });
